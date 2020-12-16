1030e12f582d61e18e45ddb833e421e48d44b313
Al-Tirah <stuartdaniells@Altirah.local>
2020-12-16 18:16:19 +0530

    removing errors

--- src/pure_components/Scroll.js
+++
@@ -1,9 +0,0 @@
-const scrollBar = (props) => {
-    return(
-        <div style={{overflowY: 'scroll', height: '72vh'}}>
-            {props.children}
-        </div>
-    )
-}
-
-export default scrollBar;
